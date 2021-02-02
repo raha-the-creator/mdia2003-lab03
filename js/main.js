c828ac5c6d59990443ac78a4e6d694678fdf7745
Rocky D <duisek.kyzylorda@gmail.com>
2021-02-03 02:15:43 +0500

    grid layout complex
    It's 2:15 AM and this water taste amazing!

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.querySelector(".mobile-nav").addEventListener("click", function(e) {
+    const element = document.querySelector(".main-nav");
+    element.classList.toggle("s-show");
+    e.preventDefault();
+}, false)
